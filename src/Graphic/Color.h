e0e2531c7cd8254cf5105996f714da7c17b6c8fa
snakeman <xjiang866@gmail.com>
2022-09-13 15:49:30 -0700

    add Color interface and implementation

---
+++ src/Graphic/Color.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <stdint.h>
+
+struct SDL_PixelFormat;
+
+class Color
+{
+	uint32_t mColor;
+public:
+
+	static const SDL_PixelFormat* mFormat;
+	static void InitColorFormat(const SDL_PixelFormat* format);
+
+	Color() : mColor(0) {}
+	Color(uint32_t color) : mColor(color) {}
+	Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
+	inline bool operator== (const Color& c) const { return mColor == c.mColor; }
+	inline bool operator!= (const Color& c) const { return !(*this == c); }
+	inline uint32_t GetPixelColor() const { return mColor; }
+
+	void SetRGBA(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+	void SetRed(uint8_t red);
+	void SetGreen(uint8_t green);
+	void SetBlue(uint8_t blue);
+	void SetAlpha(uint8_t alpha);
+
+	uint8_t GetRed() const;
+	uint8_t GetGreen() const;
+	uint8_t GetBlue() const;
+	uint8_t GetAlpha() const;
+};
--- src/Utils/main.cpp
+++ src/Utils/main.cpp
@@ -1,7 +1,71 @@
 #include "Vec2D.h"
 #include <SDL.h>
 
+const int SCREEN_WIDTH = 224;
+const int SCREEN_HEIGHT = 288;
+
+void SetPixel(SDL_Surface* noptrWindowSurface, uint32_t color, int x, int y);
+size_t GetIndex(SDL_Surface* noptrSurface, int r, int c);
+
 int main(int argc, char* argv[])
 {
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		std::cout << "Error SDL_Init Failed" << std::endl;
+		return 1;
+	}
+
+	SDL_Window* optrWindow = SDL_CreateWindow("Arcade",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+
+	if (!optrWindow)
+	{
+		std::cout << "Could not create window, got error: " << SDL_GetError() << std::endl;
+	}
+
+	SDL_Surface* noptrWindowSurface = SDL_GetWindowSurface(optrWindow);
+
+	uint32_t color = 0xFF0000;
+	SDL_UpdateWindowSurface(optrWindow);
+
+	SDL_Event sdlEvent;
+	bool running = true;
+
+	while (running)
+	{
+		while (SDL_PollEvent(&sdlEvent))
+		{
+			switch (sdlEvent.type)
+			{
+			case SDL_QUIT:
+				running = false;
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(optrWindow);
+	SDL_Quit();
+
 	return 0;
+}
+
+void SetPixel(SDL_Surface* noptrWindowSurface, uint32_t color, int x, int y)
+{
+	SDL_LockSurface(noptrWindowSurface);
+
+	uint32_t* pixels = (uint32_t*)noptrWindowSurface->pixels;
+	size_t index = GetIndex(noptrWindowSurface, y, x);
+	pixels[index] = color;
+
+	SDL_UnlockSurface(noptrWindowSurface);
+}
+
+size_t GetIndex(SDL_Surface* noptrSurface, int r, int c)
+{
+	return r * noptrSurface->w + c;
 }
