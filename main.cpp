cec2c9b87402fe15c3b52ce880565b3c3708800c
snakeman <xjiang866@gmail.com>
2022-09-15 16:49:14 -0700

    add App class

--- main.cpp
+++ main.cpp
@@ -1,44 +1,18 @@
-#include <SDL.h>
-#include "Screen.h";
-#include "Line2D.h"
-#include "Triangle.h"
-#include "AARectangle.h"
-#include "Circle.h"
-
+#include "App.h"
+#include <iostream>
 const int SCREEN_WIDTH = 224;
 const int SCREEN_HEIGHT = 288;
 const int MAGNIFICATION = 2;
 
 int main(int argc, char* argv[])
 {
-	Screen theScreen;
-	theScreen.Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION);
-
-	Circle circle(Vec2D(theScreen.Width() / 2.0f, theScreen.Height() / 2.0f), 50.0f);
-	Triangle triangle(Vec2D(50, 50), Vec2D(100, 30), Vec2D(25, 100));
-	AARectangle rect(Vec2D(150, 30), Vec2D(200, 200));
-
-	theScreen.Draw(rect, Color::White(), true, Color::Blue());
-	theScreen.Draw(circle, Color(0, 255, 0, 150), true, Color(0, 255, 0, 150));
-	theScreen.SwapScreen();
-
-	SDL_Event sdlEvent;
-	bool running = true;
-
-	while (running)
+	if (App::Singleton().Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION))
 	{
-		while (SDL_PollEvent(&sdlEvent))
-		{
-			switch (sdlEvent.type)
-			{
-			case SDL_QUIT:
-				running = false;
-				break;
-
-			default:
-				break;
-			}
-		}
+		App::Singleton().Run();
+	}
+	else
+	{
+		std::cout << "the App failed to call init()" << std::endl;
 	}
 	return 0;
 }
---
+++ src/App/App.cpp
@@ -0,0 +1,76 @@
+#include "App.h"
+#include <SDL.h>
+#include "Screen.h";
+#include "Line2D.h"
+#include "Triangle.h"
+#include "AARectangle.h"
+#include "Circle.h"
+
+
+App& App::Singleton()
+{
+	static App theApp;
+	return theApp;
+}
+
+bool App::Init(uint32_t width, uint32_t height, uint32_t mag)
+{
+	mnoptrWindow = mScreen.Init(width, height, mag);
+	return mnoptrWindow != nullptr;
+}
+
+void App::Run()
+{
+	if (mnoptrWindow)
+	{
+		Circle circle(Vec2D(mScreen.Width() / 2.0f, mScreen.Height() / 2.0f), 50.0f);
+		Triangle triangle(Vec2D(50, 50), Vec2D(100, 30), Vec2D(25, 100));
+		AARectangle rect(Vec2D(150, 30), Vec2D(200, 200));
+
+		SDL_Event sdlEvent;
+		bool running = true;
+
+		uint32_t lastTick = SDL_GetTicks();
+		uint32_t currentTick = lastTick;
+		uint32_t dt = 10;
+		uint32_t accumulator = 0;
+
+		while (running)
+		{
+			currentTick = SDL_GetTicks();
+			uint32_t frameTime = currentTick - lastTick;
+			if (frameTime > 300) frameTime = 300;
+			lastTick = currentTick;
+
+			accumulator += frameTime;
+
+			// Input
+			while (SDL_PollEvent(&sdlEvent))
+			{
+				switch (sdlEvent.type)
+				{
+				case SDL_QUIT:
+					running = false;
+					break;
+
+				default:
+					break;
+				}
+			}
+
+			// Update
+			while (accumulator >= dt)
+			{
+				// update current scene by dt;
+				std::cout << "Delta time step: " << dt << std::endl;
+
+				accumulator -= dt;
+			}
+
+			// Render
+			mScreen.Draw(rect, Color::White(), true, Color::Blue());
+			mScreen.Draw(circle, Color(0, 255, 0, 150), true, Color(0, 255, 0, 150));
+			mScreen.SwapScreen();
+		}
+	}
+}
