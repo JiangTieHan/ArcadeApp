6351c523e3d6727800c23c26fd68b50885aef37e
snakeman <xjiang866@gmail.com>
2022-09-15 17:02:59 -0700

    add Scene Interface

---
+++ src/Scenes/Scene.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+class Screen;
+
+// Interface
+class Scene
+{
+public:
+	virtual ~Scene() {}
+	virtual void Init() = 0;
+	virtual void Update(uint32_t dt) = 0;
+	virtual void Draw(Screen& theScreen) = 0;
+	virtual const std::string& GetSceneName() const = 0;
+};
